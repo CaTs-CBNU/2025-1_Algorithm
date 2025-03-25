f457b474cce08f4b0da832cbeb7d46dbaae9025f
cbnuLeehyunwoo <henow123@naver.com>
2025-03-25 21:38:56 +0900

    백준 2442번 문제풀이

---
+++ 2021076046_이현우/Week01_ 기본입출력/2442.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N = 0;
+    int idx = 0;
+    cin >> N;
+    for (int i = N; i > 0; i--) {
+        for (int j = 1; j < i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * idx + 1; k++) {
+            cout << "*";
+        }
+        index++;
+        cout << "\n";
+    }
+ 	return 0;
+}
