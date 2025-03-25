f5463f249142792bfdf48bb70cd8face2b804d3e
cbnuLeehyunwoo <henow123@naver.com>
2025-03-25 21:40:11 +0900

    백준 2446번 문제풀이

---
+++ 2021076046_이현우/Week01_ 기본입출력/2446.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+
+		for (int k = 0; k < i; k++) {
+			cout << " ";
+		}
+
+		for (int j = 2 * (n - i - 1); j >= 0; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = n - i; j > 1; j--) {
+			cout << " ";
+		}
+
+		for (int k = 0; k <= 2 * i; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
