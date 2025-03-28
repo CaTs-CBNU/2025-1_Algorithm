4c21475c358c3108997a7aab842a7adf3109dc90
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-03-28 22:02:02 +0900

    백준 11653번 문제풀이

--- 2022040037_인민혁/Week02_Math/11653.cpp
+++ 2022040037_인민혁/Week02_Math/11653.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
 
-void Eratos(int n) {
-	vector<bool> is_prime(n + 1, true);
-	is_prime[1] = false;
-	for (int i = 2; i < sqrt(n); i++) {
-		if (is_prime) {
-			for (int j = pow(i, 2); j <= n; j++) {
-				is_prime[j] = false;
-			}
-		}
-	}
-	for (int i = 2; i <= n; i++) {
-		if (is_prime[i]) cout << i << '\n';
-	}
-}
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	int num;
-	cin >> num;
-	Eratos(num);
+    int num;
+    cin >> num;
+    int i = 2;
+    while (num != 1) {
+        if (num % i == 0) {
+            num /= i;
+            cout << i << '\n';
+        }
+        else i++;
+    }
 }
