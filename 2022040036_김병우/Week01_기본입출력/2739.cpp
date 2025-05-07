de652d403fe60c2ee3295aa4f0fa64193d9ee5d9
xnoa03 <xnoa03@naver.com>
2025-05-08 04:15:32 +0900

    백준 2743번 문제풀이

--- 2022040036_김병우/Week01_기본입출력/2739.cpp
+++ 2022040036_김병우/Week01_기본입출력/2739.cpp
@@ -8,7 +8,7 @@ int main()
 	cin.tie(0);
 	int n;
 	cin >> n;
-	for (int i = 1;i <= n;i++) {
-		cout << n<<' * '<<i<<' = '<<n*i<<'\n';
+	for (int i = 1;i <= 9;i++) {
+		cout << n << " * " << i << " = " << n * i << '\n';
 	}
 }
