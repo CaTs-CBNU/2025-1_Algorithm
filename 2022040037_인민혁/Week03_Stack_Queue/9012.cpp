78511187c1197518df2faf828c83cd33b093846b
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-05-11 15:24:20 +0900

    백준 9012번 문제풀이

---
+++ 2022040037_인민혁/Week03_Stack_Queue/9012.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        string paren;
+        cin >> paren;
+        int cnt = 0;
+        bool isVPS = true;
+        for (char c : paren) {
+            if (c == '(')
+                cnt++;
+            else if (c == ')') {
+                cnt--;
+                if (cnt < 0) {
+                    isVPS = false;
+                    break;
+                }
+            }
+        }
+        if (isVPS && cnt == 0)
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << '\n';
+    }
+}
