3c4e4b4b7b4c9e5c517cd417f0514ef87fc4ca23
dlsalsgur03 <dlsalsgur2@gmail.com>
2025-05-11 14:45:20 +0900

    백준 10828번 문제풀이

---
+++ 2022040037_인민혁/Week03_Stack_Queue/10828.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Stack {
+    int ind;
+    vector<int> arr;
+public:
+    Stack() {
+        ind = 0;
+        arr.resize(ind);
+    }
+    void push(int n) {
+        arr.resize(ind + 1);
+        arr[ind++] = n;
+    }
+    void pop() {
+        if (ind <= 0) cout << -1 << '\n';
+        else cout << arr[--ind] << '\n';
+    }
+    void size() {
+        cout << ind << '\n';
+    }
+    void empty() {
+        if (ind <= 0) cout << 1 << '\n';
+        else cout << 0 << '\n';
+    }
+    void top() {
+        if (ind <= 0) cout << -1 << '\n';
+        else cout << arr[ind - 1] << '\n';
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    Stack S;
+    for (int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+        if (command == "push") {
+            int num;
+            cin >> num;
+            S.push(num);
+        }
+        else if (command == "pop")
+            S.pop();
+        else if (command == "size")
+            S.size();
+        else if (command == "empty")
+            S.empty();
+        else if (command == "top")
+            S.top();
+    }
+}
