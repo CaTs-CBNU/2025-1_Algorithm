a62481b47d6f25df1bc0c8400d16c78f57f5c42a
yuntae <jungryeok168@naver.com>
2025-05-18 17:15:28 +0900

    백준 11729번 문제풀이

---
+++ 2022040033_김윤태/Week04_큐_재귀/11729.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <queue>
+#include <math.h>
+
+using namespace std;
+
+void hanoi(int a, int b, int n) {
+	if (n == 1) {
+		cout << a << ' ' << b << "\n";
+		return;
+	}
+	hanoi(a, 6 - a - b, n - 1);
+	cout << a << ' ' << b << "\n";
+	hanoi(6 - a - b, b, n - 1);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int k;
+	cin >> k;
+	cout << int(pow(2, k) - 1) << '\n';
+	hanoi(1, 3, k);
+
+}
